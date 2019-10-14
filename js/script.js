77b90a73e47bd2296ed15db81beef4774e1ccdb0
Peter Slezak <piotrek.slezak@gmail.com>
2019-10-14 16:08:29 +0200

    added classList active for clicked href

--- js/script.js
+++ js/script.js
@@ -1,28 +1,36 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;  
   console.log('Link was clicked!');
   console.log(event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
   const activeLinks = document.querySelectorAll('.titles a.active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
-  /* add class 'active' to the clicked link */
 
-  /* remove class 'active' from all articles */
+  /* [IN PROGRESS] add class 'active' to the clicked link */
+
+  console.log('clickedElement:', clickedElement);
+
+  /* [DONE] remove class 'active' from all articles */
+
   const activeArticles = document.querySelectorAll('.posts .active');
 
   for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
   }
+
   /* get 'href' attribute from the clicked link */
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+  clickedElement.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
@@ -30,3 +38,4 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
