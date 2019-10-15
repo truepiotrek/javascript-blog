b4fa8324dd98b4e9084c021b11c52ad0363e1dde
Peter Slezak <piotrek.slezak@gmail.com>
2019-10-15 20:01:20 +0200

    finalised article selector function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+  event.preventDefault();
   const clickedElement = this;  
   console.log('Link was clicked!');
-  console.log(event);
+  // console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -13,7 +14,7 @@ function titleClickHandler(event){
     activeLink.classList.remove('active');
   }
 
-  /* [IN PROGRESS] add class 'active' to the clicked link */
+  /* [DONE] add class 'active' to the clicked link */
 
   console.log('clickedElement:', clickedElement);
 
@@ -25,12 +26,20 @@ function titleClickHandler(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute("href");
+  console.log(articleSelector);
 
-  /* add class 'active' to the correct article */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+  
+
+  /* [DONE] add class 'active' to the correct article */
   clickedElement.classList.add('active');
+  targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
