fa51bb2db655c75896553f1c05df5f1f36f13a22
Peter Slezak <piotrek.slezak@gmail.com>
2019-10-15 21:01:44 +0200

    updated script.js to delete titles in left column

--- js/script.js
+++ js/script.js
@@ -48,3 +48,31 @@ for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
 
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+
+function generateTitleLinks(){
+
+  /* remove contents of titleList */
+
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  
+
+  /* for each article */
+
+    /* get the article id */
+
+    /* find the title element */
+
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+
+    /* insert link into titleList */
+
+}
+
+generateTitleLinks();
