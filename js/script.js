169c464c8858070d0aab7213ceef21507b16971d
Peter Slezak <piotrek.slezak@gmail.com>
2019-10-16 12:41:06 +0200

    created JS link list with working active attribute

--- js/script.js
+++ js/script.js
@@ -42,12 +42,6 @@ function titleClickHandler(event){
   targetArticle.classList.add('active');
 }
 
-const links = document.querySelectorAll('.titles a');
-
-for(let link of links){
-  link.addEventListener('click', titleClickHandler);
-}
-
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
@@ -56,23 +50,42 @@ const optArticleSelector = '.post',
 function generateTitleLinks(){
 
   /* remove contents of titleList */
-
   const titleList = document.querySelector(optTitleListSelector);
   titleList.innerHTML = '';
   
+  let html='';
 
   /* for each article */
-
+  const articles = document.querySelectorAll(optArticleSelector);
+  for(let article of articles){
+    
     /* get the article id */
+    const articleId = article.getAttribute('id');
 
     /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
 
     /* get the title from the title element */
+    
 
     /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    // console.log(linkHTML);
 
     /* insert link into titleList */
+    // titleList.insertAdjacentHTML('beforebegin', linkHTML);
+    html = html + linkHTML;
+    // console.log(html);
+  }
 
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+  console.log(links);
+
+  for(let link of links){
+  link.addEventListener('click', titleClickHandler);
+}
 }
 
 generateTitleLinks();
